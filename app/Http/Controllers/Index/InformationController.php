af2b52223f895f11d08505c7a215666f750732e6
caolu <1874836085@qq.com>
2020-11-11 17:02:16 +0800

    cao

--- app/Http/Controllers/Index/InformationController.php
+++ app/Http/Controllers/Index/InformationController.php
@@ -9,20 +9,20 @@ class InformationController extends Controller
 {
     //热门资讯
     public function informationhot(){
-    	$hot=Information::where('infor_hot',1000)->limit(10)->get();
+    	$hot=Information::where('infor_hot','>',1000)->limit(20)->get();
     	$infor_time=time();
     	return view("/Index/information/informationhot",["hot"=>$hot]);
     }
     //资讯详情
     public function information(){
-    	$a=Information::where('infor_hot',988777)->limit(1)->get();
+    	$a=Information::where('infor_hot',3445)->limit(1)->get();
     	$hot=Information::where('infor_hot',1000)->limit(5)->get();
     	return view("/Index/information/information",["a"=>$a,"hot"=>$hot]);
     }
     //资讯
     public function informationlist(){
-    	$list=Information::where('infor_id',22)->limit(5)->get();
-    	$hot=Information::where('infor_hot',1000)->limit(5)->get();   	
+    	$list=Information::limit(5)->get();
+    	$hot=Information::where('infor_hot','>',1000)->limit(5)->get();   	
     	return view("/Index/information/informationlist",["list"=>$list,"hot"=>$hot]);
     }
 }
