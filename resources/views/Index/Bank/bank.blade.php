587d5f1e8776a313b7da7b7eab65639f8e0c7318
lxk <964673672@qq.com>
2020-11-12 14:26:54 +0800

    1

--- resources/views/Index/Bank/bank.blade.php
+++ resources/views/Index/Bank/bank.blade.php
@@ -2,9 +2,9 @@
 @section('app.name', '课程')
 @section('content')
 <link rel="stylesheet" href="/static/index/css/article.css">
-<script src="../../../../../static/index/js/jquery-1.8.0.min.js"></script>
-<script src="../../../../../static/index/js/mine.js"></script>
-<script type="text/javascript" src="../../../static/index/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+<script src="/static/index/js/jquery-1.8.0.min.js"></script>
+<script src="/static/index/js/mine.js"></script>
+<script type="text/javascript" src="/static/index/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
 <!-- InstanceBeginEditable name="EditRegion1" -->
 <div class="coursecont">
 <div class="coursepic">
@@ -71,7 +71,7 @@
     <div class="teacher">
         @foreach($course_tj as $k=>$v)
     <div class="teapic">
-        <a href="#"  target="_blank"><img src="../../../../../static/index/images//c1.jpg" height="60" title="财经法规与财经职业道德"></a>
+        <a href="#"  target="_blank"><img src="/static/index/images/c1.jpg" height="60" title="财经法规与财经职业道德"></a>
         <h3 class="courh3"><a href="JavaScript:;" class="ask_link cou_detail" cou_id="{{$v->cou_id}}">{{$v->cou_name}}</a></h3>
     </div>
     <div class="clearh"></div> @endforeach
